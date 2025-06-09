e56699ca1bb6cf2e3142cb8224915691a13f2a5e
Kanhaiya Yadav <kanhaiya2004yadav@gmail.com>
2025-06-09 17:50:57 +0530

    created ui for signin and signup page

--- app/(auth)/signin.tsx
+++ app/(auth)/signin.tsx
@@ -1,8 +1,6 @@
+// app/(auth)/signin.tsx
 import React from 'react';
-import { images } from '@/constants/images';
-import { Link, useRouter } from 'expo-router';
-import { Image, Pressable, Text, TextInput, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Pressable, Text, TextInput, View } from 'react-native';
 import Fontisto from '@expo/vector-icons/Fontisto';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { Button } from '@/components/ui/button';
@@ -12,53 +10,52 @@ import {
   CheckboxLabel,
   CheckboxIcon,
 } from "@/components/ui/checkbox"
+import { Link, useRouter } from 'expo-router';
 
 const SignIn = () => {
-
-  const router = useRouter()
+  const router = useRouter();
 
   return (
-    <View className='bg-primary-500/5 flex-1 w-screen relative'>
-      <View className='w-full aspect-[298/217]'>
-        <Image
-          source={images.authBg}
-          className='w-full h-full'
-          style={{ resizeMode: 'cover' }}
-        />
-      </View>
-      <View className='flex-1 relative'>
-        <View className='w-full aspect-[1440/500] absolute top-0 left-0 translate-y-[-98%]'>
-          <Image
-            source={images.wave}
-            className='w-full h-full'
-          />
+    <View className='flex-1'>
+      {/* Page Heading */}
+      <Text className='text-5xl leading-[50px] font-bold pl-8 translate-y-[-10%] mb-6'>
+        Sign In
+      </Text>
+
+      {/* Form Content */}
+      <View className='px-8 gap-6'>
+        <View className='flex flex-row items-center gap-4 border-b border-primary-500 py-2 px-2'>
+          <Fontisto name="email" size={24} color={"gray"} />
+          <TextInput keyboardType='email-address' className='flex-1 text-lg p-0 m-0' placeholder='Email' />
         </View>
-        <Text className='text-5xl leading-[50px] font-bold pl-8 translate-y-[-100%]'>Sign in</Text>
-        <View className='px-8 gap-6'>
-          <View className='flex flex-row items-center gap-4 border-b border-primary-500 py-2 px-2'>
-            <Fontisto name="email" size={24} color={"gray"} />
-            <TextInput className='flex-1 text-lg p-0 m-0' placeholder='Email' />
-          </View>
-          <View className='flex flex-row items-center gap-4 border-b border-primary-500 py-2 px-2'>
-            <Ionicons name="lock-closed-outline" size={24} color="gray" />
-            <TextInput className='flex-1 text-lg p-0 m-0' placeholder='Password' />
-          </View>
-          <View className='flex flex-row items-center justify-between mt-[-5px]'>
-            <Checkbox value='something' size="sm" isInvalid={false} isDisabled={false} className='ml-2'>
-              <CheckboxIndicator>
-                <CheckboxIcon />
-              </CheckboxIndicator>
-              <CheckboxLabel>Remember me</CheckboxLabel>
-            </Checkbox>
-            <Link href={'/(auth)/forgot-password'} asChild className='ml-auto'>
-              <Pressable className='mt-2'>
-                <Text className='text-primary-500 text-sm'>Forgot Password?</Text>
-              </Pressable>
-            </Link>
-          </View>
-          <Button size='xl' className='h-[50px]'>
-            <Text className='text-lg text-primary-foreground'>Sign In</Text>
-          </Button>
+        <View className='flex flex-row items-center gap-4 border-b border-primary-500 py-2 px-2'>
+          <Ionicons name="lock-closed-outline" size={24} color="gray" />
+          <TextInput className='flex-1 text-lg p-0 m-0' placeholder='Password' />
+        </View>
+        <View className='flex flex-row items-center justify-between mt-[-5px]'>
+          <Checkbox value='something' size="sm" isInvalid={false} isDisabled={false} className='ml-2'>
+            <CheckboxIndicator>
+              <CheckboxIcon />
+            </CheckboxIndicator>
+            <CheckboxLabel>Remember me</CheckboxLabel>
+          </Checkbox>
+          <Link href={'/(auth)/forgot-password'} asChild className='ml-auto'>
+            <Pressable className='mt-2'>
+              <Text className='text-primary-500 text-sm'>Forgot Password?</Text>
+            </Pressable>
+          </Link>
+        </View>
+        <Button size='xl' className='h-[50px]'>
+          <Text className='text-lg text-primary-foreground'>Sign In</Text>
+        </Button>
+        <View className='flex flex-row items-center gap-1 w-fit m-auto mt-[-10px]'>
+          <Text>Don&apos;t have an account?</Text>
+          <Text
+            className='font-bold text-primary-500'
+            onPress={() => router.push('/(auth)/signup')}
+          >
+            Sign Up
+          </Text>
         </View>
       </View>
     </View>
--- app/(auth)/signup.tsx
+++ app/(auth)/signup.tsx
@@ -1,12 +1,55 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { Pressable, Text, TextInput, View } from 'react-native';
+import Fontisto from '@expo/vector-icons/Fontisto';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { Button } from '@/components/ui/button';
+import {
+  Checkbox,
+  CheckboxIndicator,
+  CheckboxLabel,
+  CheckboxIcon,
+} from "@/components/ui/checkbox"
+import { Link, useRouter } from 'expo-router';
 
-const signup = () => {
+const SignUp = () => {
+  const router = useRouter();
+
   return (
-    <View>
-      <Text>signup</Text>
+    <View className='flex-1'>
+      {/* Page Heading */}
+      <Text className='text-5xl leading-[50px] font-bold pl-8 translate-y-[-10%] mb-6'>
+        Sign Up
+      </Text>
+
+      {/* Form Content */}
+      <View className='px-8 gap-6'>
+        <View className='flex flex-row items-center gap-4 border-b border-primary-500 py-2 px-2'>
+          <Fontisto name="email" size={24} color={"gray"} />
+          <TextInput className='flex-1 text-lg p-0 m-0' placeholder='Email' />
+        </View>
+        <View className='flex flex-row items-center gap-4 border-b border-primary-500 py-2 px-2'>
+          <Ionicons name="lock-closed-outline" size={24} color="gray" />
+          <TextInput className='flex-1 text-lg p-0 m-0' placeholder='Password' />
+        </View>
+        <View className='flex flex-row items-center gap-4 border-b border-primary-500 py-2 px-2'>
+          <Ionicons name="lock-closed-outline" size={24} color="gray" />
+          <TextInput className='flex-1 text-lg p-0 m-0' placeholder='Confirm Password' />
+        </View>
+        <Button size='xl' className='h-[50px]'>
+          <Text className='text-lg text-primary-foreground'>Sign Up</Text>
+        </Button>
+        <View className='flex flex-row items-center gap-1 w-fit m-auto mt-[-10px]'>
+          <Text>Already have an account?</Text>
+          <Text
+            className='font-bold text-primary-500'
+            onPress={() => router.navigate('/(auth)/signin')}
+          >
+            Sign In
+          </Text>
+        </View>
+      </View>
     </View>
   )
 }
 
-export default signup
+export default SignUp
